051c5da139a90bf55bd80fa9c0f7cb524ff349bf
Aryan-GH7 <pallaviaryangupta7@gmail.com>
2026-02-04 20:16:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+*{
+    padding: 0;
+    margin: 0;
+}
+.navbar{
+    background-color: rgb(116, 116, 116);
+}
+.navbar-brand{
+    padding: 0;
+    display: flex;
+    align-items: center;
+}
+.navbar{
+    padding: 0;
+}
 
